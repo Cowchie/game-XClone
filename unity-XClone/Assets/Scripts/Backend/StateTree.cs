907da86df1be6ab3a6d64734cf02ad61e8394173
Cowchie <gigliotticole@gmail.com>
2023-01-16 10:26:21 -0500

    Changed tree branches to accept tree structure and tree specialization functions.

--- unity-XClone/Assets/Scripts/Backend/StateTree.cs
+++ unity-XClone/Assets/Scripts/Backend/StateTree.cs
@@ -1,47 +1,60 @@
 namespace StateTree{
 
-public interface TreeBranch{
-    public void CenterOn(TreeBranch prev);
-    public TreeBranch DoUpdate();
+// The signature of a function which specializes a tree to one of its branches.
+public delegate TreeBranch<TreeS> GetBranch<TreeS>(TreeS tree);
+
+// The interface from which all tree branches derive. 
+public interface TreeBranch<TreeS>{
+    public void CenterOn(GetBranch<TreeS> prev, TreeS tree);
+    public TreeBranch<TreeS> DoUpdate(TreeS tree);
 }
 
-public class CycleBranches : TreeBranch{
-    private TreeBranch[] branches;
+// A branch which cycles through an array of branches
+public class CycleBranches<TreeS> : TreeBranch<TreeS>{
+    private GetBranch<TreeS>[] getsBranches;
     private int current_index;
 
-    public CycleBranches(params TreeBranch[] branch_array){
-        branches = branch_array;
+    public CycleBranches(params GetBranch<TreeS>[] get_branch_array){
+        getsBranches = get_branch_array;
         current_index = -1;
     }
 
-    void TreeBranch.CenterOn(TreeBranch prev){
+    void TreeBranch<TreeS>.CenterOn(GetBranch<TreeS> prev, TreeS tree){
         current_index++;
-        current_index = current_index < branches.Length ? current_index : 0;
+        current_index = current_index < getsBranches.Length ? current_index : 0;
     }
 
-    TreeBranch TreeBranch.DoUpdate(){
-        return branches[current_index];
+    TreeBranch<TreeS> TreeBranch<TreeS>.DoUpdate(TreeS tree){
+        return (getsBranches[current_index])(tree);
     }
 }
 
-public delegate void WhenCenteredOnDelegate(
-        TreeBranch previous, 
-        TreeBranch current
+// The signature of a function which is called everytime we center on CallbacksThenDoNext.
+public delegate void WhenCenteredOn<TreeS>(
+        GetBranch<TreeS>    previous, 
+        GetBranch<TreeS>    current,
+        TreeS               tree
     );
-public class CallbacksThenDoNext : TreeBranch{
-    public event WhenCenteredOnDelegate OnCenterOn;
+// A branch which calls a list of callbacks everytime it is centered on and then immediately moves on to another branch.
+public class CallbacksThenDoNext<TreeS> : TreeBranch<TreeS>{
+    public event WhenCenteredOn<TreeS> OnCenterOn;
 
-    private TreeBranch next;
-    public CallbacksThenDoNext(TreeBranch next_branch){
-        next = next_branch;
+    private GetBranch<TreeS> get_this;
+    private GetBranch<TreeS> get_next;
+    public CallbacksThenDoNext(
+        GetBranch<TreeS> this_branch, 
+        GetBranch<TreeS> next_branch
+    ){
+        get_this = this_branch;
+        get_next = next_branch;
     }
 
-    void TreeBranch.CenterOn(TreeBranch prev){
-        OnCenterOn?.Invoke(prev, this);
+    void TreeBranch<TreeS>.CenterOn(GetBranch<TreeS> prev, TreeS tree){
+        OnCenterOn?.Invoke(prev, get_this, tree);
     }
 
-    TreeBranch TreeBranch.DoUpdate(){
-        return next;
+    TreeBranch<TreeS> TreeBranch<TreeS>.DoUpdate(TreeS tree){
+        return get_next(tree);
     }
 }
 
